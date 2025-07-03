216759b57d7e5422196d084fca70695800710e83
Renan Sousa <44421770+RenanSousa-96@users.noreply.github.com>
2025-07-03 17:45:59 -0300

    Update cleaned_d_time.sql

---
+++ 05. Materializações, Testes Avançados e Implementação/owsbank/models/cleaned_d_time.sql
@@ -0,0 +1,11 @@
+select
+    time_id,
+    {% if target.type == 'postgres' %}
+        action_timestamp:: timestamp
+    {% elif target.type == 'bigquery' -%}
+        timestamp(action_timestamp)
+    {%- else -%}
+        action_timestamp
+    {%- endif -%}
+    as action_timestamp
+from {{ source('postgres', 'd_time') }}
